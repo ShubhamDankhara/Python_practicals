5e6142a6bcbdb3eeee159ea953eead071395d37b
SHUBHAMDANKHARA <shubhamdankhara007@gmail.com>
2022-02-26 21:57:16 +0530

    Practicals
    Practical-5,6,7

---
+++ Set/set_1.py
@@ -0,0 +1,10 @@
+# Write a Python program to add member(s) in a set and clear a set
+# Shubham Dankhara 
+# D21cS108
+
+odd_num={1,3,5,7}
+print(odd_num)
+odd_num.add(9)
+print(odd_num)
+odd_num.clear()
+print(odd_num)
